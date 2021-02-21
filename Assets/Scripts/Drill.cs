b621896194ca3196072a9c0d77ea3001d591e514
Linkraft <jordan.smiths.games@gmail.com>
2021-02-21 17:17:21 -0500

    Added tooth models and working on drilling

---
+++ Assets/Scripts/Drill.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drill : MonoBehaviour
+{
+    public AudioClip drillAudio;
+    private AudioSource audioPlayer;
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        audioPlayer.PlayOneShot(drillAudio);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        /*
+        audioPlayer.PlayOneShot(drillAudio);
+
+        if (other.CompareTag("Level1"))
+            HapticManager.singleton.TriggerVibration(255,255,255, OVRInput.Controller.RTouch);
+
+        if (other.CompareTag("Level2"))
+            HapticManager.singleton.TriggerVibration(127,127, 127, OVRInput.Controller.RTouch);
+        
+        if (other.CompareTag("Level3"))
+            HapticManager.singleton.TriggerVibration(63,63,63, OVRInput.Controller.RTouch);
+        */
+
+        //HapticManager.singleton.TriggerVibration(drillAudio, OVRInput.Controller.RTouch);
+        HapticManager.singleton.TriggerVibration(40, 2, 255 , OVRInput.Controller.RTouch);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        audioPlayer.Stop();
+        //OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
+    }
+}
---
+++ Assets/Scripts/HapticManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HapticManager : MonoBehaviour
+{
+    public static HapticManager singleton;
+    void Start()
+    {
+        if (singleton && singleton != this)
+            Destroy(this);
+        else
+            singleton = this;
+    }
+
+    //Vibrate with audio file
+    public void TriggerVibration(AudioClip vibrationAudio, OVRInput.Controller controller)
+    {
+        OVRHapticsClip clip = new OVRHapticsClip(vibrationAudio);
+
+        if(controller == OVRInput.Controller.RTouch)
+        {
+            OVRHaptics.RightChannel.Mix(clip);
+        }
+
+    }
+
+    //Vibrate without audio file
+    public void TriggerVibration(int iteration, int frequency, int strength, OVRInput.Controller controller)
+    {
+        OVRHapticsClip clip = new OVRHapticsClip();
+        for (int i = 0; i<iteration; i++){
+            clip.WriteSample(i % frequency == 0 ? (byte)strength : (byte)0);
+        }
+
+        if (controller == OVRInput.Controller.RTouch)
+        {
+            OVRHaptics.RightChannel.Preempt(clip);
+        }
+
+    }
+
+}
